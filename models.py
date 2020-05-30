b0d446de0a274949fa10b6535d4447e8c8a8796b
Baher Wael <bwy@crossworkers.com>
2020-05-31 01:22:25 +0200

    Implemet app models

---
+++ models.py
@@ -0,0 +1,84 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config')
+    db.init_app(app)
+    return (app, db)
+
+
+class Genre(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(), unique=True)
+
+
+venue_genre = db.Table('venue_genre',
+    db.Column('venue_id', db.Integer, db.ForeignKey('venue.id'), primary_key=True),
+    db.Column('genre_id', db.Integer, db.ForeignKey('genre.id'), primary_key=True)
+)
+
+
+artist_genre = db.Table('artist_genre',
+    db.Column('artist_id', db.Integer, db.ForeignKey('artist.id'), primary_key=True),
+    db.Column('genre_id', db.Integer, db.ForeignKey('genre.id'), primary_key=True)
+)
+
+
+class State(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(2), unique=True)
+  venues = db.relationship('Venue', backref='state', lazy='joined')
+  artists = db.relationship('Artist', backref='state', lazy='joined')
+
+
+class Venue(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    city = db.Column(db.String(120))
+    state_id = db.Column(db.Integer, db.ForeignKey('state.id'))
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
+    website = db.Column(db.String())
+    seeking_talent = db.Column(db.Boolean, default=False)
+    seeking_description = db.Column(db.String())
+    past_shows = db.relationship('Show', backref="venue_past_shows", lazy=True)
+    upcoming_shows = db.relationship('Show', backref="venue_upcoming_shows", lazy=True)
+    past_shows_count = db.Column(db.Integer)
+    upcoming_shows_count = db.Column(db.Integer)
+
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+
+
+class Artist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    city = db.Column(db.String(120))
+    state_id = db.Column(db.Integer, db.ForeignKey('state.id'))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    website = db.Column(db.String())
+    seeking_venue = db.Column(db.Boolean, default=False)
+    seeking_description = db.Column(db.String())
+    past_shows = db.relationship('Show', backref="artists_past_shows", lazy=True)
+    upcoming_shows = db.relationship('Show', backref="artists_upcoming_shows", lazy=True)
+    past_shows_count = db.Column(db.Integer)
+    upcoming_shows_count = db.Column(db.Integer)
+
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+
+
+# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+class Show(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  venue_id = db.Column(db.Integer, db.ForeignKey('venue.id'))
+  artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'))
+  start_time = db.Column(db.DateTime)
