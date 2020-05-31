57454d4ccbe156d997b458da74c52693b87a5815
Baher Wael <bwy@crossworkers.com>
2020-05-31 23:50:56 +0200

    create show submission

--- app.py
+++ app.py
@@ -499,12 +499,27 @@ def create_shows():
 def create_show_submission():
   # called to create new shows in the db, upon submitting new show listing form
   # TODO: insert form data as a new Show record in the db, instead
-
+  error = False
+  try:
+    show = Show()
+    show.start_time = request.form['start_time']
+    venue = Venue.query.filter_by(id=request.form['venue_id']).first()
+    venue.shows.append(show)
+    artist = Artist.query.filter_by(id=request.form['artist_id']).first()
+    artist.shows.append(show)
+    db.session.commit()
+  except:
+    error = True
+    # TODO: on unsuccessful db insert, flash an error instead.
+    flash('An error occurred. Show could not be listed.')
+    db.session.rollback()
+    print(sys.exc_info())
+  finally:
+    db.session.close()
   # on successful db insert, flash success
-  flash('Show was successfully listed!')
-  # TODO: on unsuccessful db insert, flash an error instead.
-  # e.g., flash('An error occurred. Show could not be listed.')
-  # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
+  if not error:
+    flash('Show was successfully listed!')
+
   return render_template('pages/home.html')
 
 @app.errorhandler(404)
