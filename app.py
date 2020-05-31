615abd8207b02b09bbb2816a019952e486147920
Baher Wael <bwy@crossworkers.com>
2020-05-31 23:21:48 +0200

    create artist submission

--- app.py
+++ app.py
@@ -411,14 +411,35 @@ def create_artist_form():
 
 @app.route('/artists/create', methods=['POST'])
 def create_artist_submission():
-  # called upon submitting the new artist listing form
-  # TODO: insert form data as a new Venue record in the db, instead
-  # TODO: modify data to be the data object returned from db insertion
+  error = False
+  try:
+  # TODO: insert form data as a new artist record in the db, instead
+    artist = Artist()
+    artist.name = request.form['name']
+    artist.city = request.form['city']
+    state = State.query.filter_by(name=request.form['state']).first()
+    artist.state = state
+    artist.phone = request.form['phone']
+    for name in request.form.getlist('genres'):
+      genre = Genre.query.filter_by(name=name).first()
+      artist.genres.append(genre) 
+    artist.facebook_link = request.form['facebook_link']
 
+    db.session.add(artist)
+    db.session.commit()
+  except:
+    error = True
+    # TODO: on unsuccessful db insert, flash an error instead.
+    flash('An error occurred. Artist ' + artist.name + ' could not be listed.')
+    db.session.rollback()
+    print(sys.exc_info())
+  finally:
+    db.session.close()
+  # TODO: modify data to be the data object returned from db insertion
   # on successful db insert, flash success
-  flash('Artist ' + request.form['name'] + ' was successfully listed!')
-  # TODO: on unsuccessful db insert, flash an error instead.
-  # e.g., flash('An error occurred. Artist ' + data.name + ' could not be listed.')
+  if not error:
+    # on successful db insert, flash success
+    flash('Artist ' + request.form['name'] + ' was successfully listed!')
   return render_template('pages/home.html')
 
 
