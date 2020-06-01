0e538bce49b5eedc32572397f44087488685eb6c
Baher Wael <bwy@crossworkers.com>
2020-06-01 02:05:45 +0200

    Venues controller

--- app.py
+++ app.py
@@ -63,29 +63,16 @@ def index():
 def venues():
   # TODO: replace with real venues data.
   #       num_shows should be aggregated based on number of upcoming shows per venue.
-
-  data=[{
-    "city": "San Francisco",
-    "state": "CA",
-    "venues": [{
-      "id": 1,
-      "name": "The Musical Hop",
-      "num_upcoming_shows": 0,
-    }, {
-      "id": 3,
-      "name": "Park Square Live Music & Coffee",
-      "num_upcoming_shows": 1,
-    }]
-  }, {
-    "city": "New York",
-    "state": "NY",
-    "venues": [{
-      "id": 2,
-      "name": "The Dueling Pianos Bar",
-      "num_upcoming_shows": 0,
-    }]
-  }]
-  return render_template('pages/venues.html', areas=data);
+  result = State.query.with_entities(Venue.city, Venue.state_id).distinct().all()
+  newData = []
+  for city, state_id in result:
+    newData.append({
+      "city": city,
+      "state": State.query.filter_by(id=state_id).first().name,
+      "venues": Venue.query.filter_by(city=city).filter_by(state_id=state_id).all()
+    })
+    
+  return render_template('pages/venues.html', areas=newData);
 
 @app.route('/venues/search', methods=['POST'])
 def search_venues():
